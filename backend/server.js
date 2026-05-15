9ebed1035862c77252a87e9e4699a29453f477df
Greeshma Sree <yourgithubemail@gmail.com>
2026-05-15 15:59:54 +0530

    Prepare backend for Vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
-const patientRoutes = require("./routes/patientRoutes");
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
 const connectDB = require("./config/db");
+const patientRoutes = require("./routes/patientRoutes");
 
 dotenv.config();
 
@@ -13,14 +13,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/patients", patientRoutes);
 
 app.get("/", (req, res) => {
   res.send("API Running");
 });
 
-const PORT = process.env.PORT || 5000;
+app.use("/api/patients", patientRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
+module.exports = app;
